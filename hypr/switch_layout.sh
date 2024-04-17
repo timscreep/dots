b215c2cd2ba65f77fd4ca8655b8de402102aeb06
timscreep <81462085+timscreep@users.noreply.github.com>
2024-04-18 01:59:02 +0300

    Delete hypr/switch_layout.sh

--- hypr/switch_layout.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Переключить раскладку клавиатуры
-keyboard="at-translated-set-2-keyboard"
-hyprctl switchxkblayout $keyboard next
-value=$(hyprctl devices | grep -i $keyboard -A 2 | grep -i "active" | cut -d ":" -f2 )
-# Вывести уведомление о текущей раскладке
-notify-send "Current Keyboard Layout:" "${value}"
